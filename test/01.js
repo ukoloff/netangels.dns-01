76ac8cfa2125792f547d9a5d7380de13e0c18413
Stas Ukolov <ukoloff@gmail.com>
2025-08-21 05:08:41 +0000

    Add failing test

--- test/01.js
+++ test/01.js
@@ -1,4 +1,10 @@
 import { it, describe } from 'node:test'
+import assert from 'assert'
 
-it('Hello, world', x => { })
+it('Hello, world', x => {
+  assert.equal(1, 1)
+})
 
+it('Oops', async x => {
+  assert.equal(1, 2)
+})
