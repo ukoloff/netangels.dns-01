db730579b1f130a3e3621b0228deb579442efa4f
Stas Ukolov <ukoloff@gmail.com>
2025-08-22 09:04:58 +0000

    Spawn children

--- src/www.js
+++ src/www.js
@@ -1,6 +1,22 @@
 //
 // API server
 //
-export default function www(args) {
+import cluster from 'node:cluster'
+import { createServer } from 'node:http'
+import { json } from 'node:stream/consumers'
 
+export default function www(args) {
+  if (cluster.isPrimary) {
+    for (let i = 0; i < 3; i++)
+      cluster.fork()
+    cluster.on('exit', (worker, code, signal) => cluster.fork())
+    return
+  }
+  let srv = createServer(handler)
+  srv.listen(80)
+}
+
+function handler(req, res) {
+  console.log(req)
+  res.end('Hi!')
 }
