46f7e74309cc1ea8c2860fca80fdfdf25251c9b9
Stas Ukolov <ukoloff@gmail.com>
2025-08-22 08:38:30 +0000

    Test CLI cleanup

--- test/cli.js
+++ test/cli.js
@@ -9,9 +9,7 @@ describe('CLI interface', $ => {
     let name = `${await random()}.cli.uralhimmash.com`
     let value = `Hi, ${await random()}!`
     let child = spawn('node', ['.', 'present', name, value], { stdio: 'inherit' })
-    let res = await new Promise(function (resolve, reject) {
-      child.on('error', reject).on('exit', resolve)
-    })
+    let res = await wait(child)
     $.assert.equal(res, 0)
     let RRs = await remove(name, {
       type: 'TXT',
@@ -21,9 +19,32 @@ describe('CLI interface', $ => {
   })
 
   it('removes TXT RRs', async $ => {
-    let name = `${random()}.cli.uralhimmash.com`
+    let name = `${await random()}.cli.uralhimmash.com`
     let value = `Oops, ${random()}!`
-
+    let r = await create({
+      name,
+      type: 'TXT',
+      value,
+      ttl: 301,
+    })
+    $.assert.equal(r.type, 'TXT')
+    let child = spawn('node', ['.', 'cleanup', name, value], { stdio: 'inherit' })
+    let res = await wait(child)
+    $.assert.equal(res, 0)
+    let RRs = await remove(name, {
+      type: 'TXT',
+      value,
+    })
+    $.assert.equal(RRs.length, 0)
   })
 
 })
+
+
+function wait(child) {
+  return new Promise(function (resolve, reject) {
+    child
+      .on('error', reject)
+      .on('exit', resolve)
+  })
+}
