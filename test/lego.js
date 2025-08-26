9fe1e3f70ce270b7d5f95512737cdbdfba5b2e33
Stas Ukolov <ukoloff@gmail.com>
2025-08-26 06:15:27 +0000

    Start Lego tests

---
+++ test/lego.js
@@ -0,0 +1,21 @@
+import { spawn } from 'node:child_process'
+import { it, describe } from 'node:test'
+import { create, remove } from '../src/na.js'
+import random from '../src/random.js'
+
+const Staging = 'https://acme-staging-v02.api.letsencrypt.org/directory'
+
+describe('lego', lego)
+
+function lego() {
+  it('uses exec interface', exec)
+  it('uses http interface', http)
+}
+
+async function exec($) {
+
+}
+
+async function http($) {
+
+}
