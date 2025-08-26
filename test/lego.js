12787e062b9d99e6d72851afbc84a3b12bdea33c
Stas Ukolov <ukoloff@gmail.com>
2025-08-26 08:45:48 +0000

    Extract lego runner

--- test/lego.js
+++ test/lego.js
@@ -3,43 +3,19 @@ import { X509Certificate } from 'node:crypto'
 import { spawn } from 'node:child_process'
 import { it, describe } from 'node:test'
 import random from '../src/random.js'
+import assert from 'node:assert'
 
-const Staging = 'https://acme-staging-v02.api.letsencrypt.org/directory'
-
-describe('lego', lego)
-
-function lego() {
+describe('lego', $ => {
   it('uses exec interface', exec)
   it('uses http interface', http)
-}
+})
 
 async function exec($) {
-  process.env.EXEC_PATH = 'bin/na01'
-  process.env.LEGO_EMAIL = 'stas@ekb.ru'
-  process.env.LEGO_SERVER = Staging
-  process.env.EXEC_POLLING_INTERVAL = 10
-  process.env.EXEC_PROPAGATION_TIMEOUT = 300
-
-  let domain = `${await random()}.exec.uralhimmash.com`.toLowerCase()
-
-  let child = spawn('lego', [
-    '-a',
-    '-dns', 'exec',
-    '-d', domain,
-    '--pfx',
-    '--dns.resolvers', '8.8.8.8',
-    'run'],
-    { stdio: 'inherit' })
-  let res = await wait(child)
-  $.assert.equal(res, 0)
-  let crt = new X509Certificate(await fs.readFile('./.lego/certificates/' + domain + '.crt'))
-  $.assert.ok(crt.serialNumber)
-  $.assert.equal(crt.subject, 'CN=' + domain)
-  $.assert.equal(crt.subjectAltName, 'DNS:' + domain)
-
-  for (let ext of 'key pfx crt json'.split(' ')) {
-    $.assert.ok(await fs.stat('./.lego/certificates/' + domain + '.' + ext))
-  }
+  await lego('exec', {
+    EXEC_PATH: 'bin/na01',
+    EXEC_POLLING_INTERVAL: 10,
+    EXEC_PROPAGATION_TIMEOUT: 300,
+  })
 }
 
 async function http($) {
@@ -52,4 +28,33 @@ function wait(child) {
       .on('error', reject)
       .on('exit', resolve)
   })
+}
+
+async function lego(provider, env = {}) {
+  process.env.LEGO_EMAIL = 'stas@ekb.ru'
+  process.env.LEGO_SERVER = 'https://acme-staging-v02.api.letsencrypt.org/directory'
+  for (const [k, v] of Object.entries(env)) {
+    process.env[k] = v
+  }
+
+  let domain = `${await random()}.exec.uralhimmash.com`.toLowerCase()
+
+  let child = spawn('lego', [
+    '-a',
+    '-dns', provider,
+    '-d', domain,
+    '--pfx',
+    '--dns.resolvers', '8.8.8.8',
+    'run'],
+    { stdio: 'inherit' })
+  let res = await wait(child)
+  assert.equal(res, 0)
+  let crt = new X509Certificate(await fs.readFile('./.lego/certificates/' + domain + '.crt'))
+  assert.ok(crt.serialNumber)
+  assert.equal(crt.subject, 'CN=' + domain)
+  assert.equal(crt.subjectAltName, 'DNS:' + domain)
+
+  for (let ext of 'key pfx crt json'.split(' ')) {
+    assert.ok(await fs.stat('./.lego/certificates/' + domain + '.' + ext))
+  }
 }
