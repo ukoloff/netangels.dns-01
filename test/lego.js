84c0054ce6b852ea8faff92a6ee3b015017dd4dd
Stas Ukolov <ukoloff@gmail.com>
2025-08-26 06:40:56 +0000

    Run Lego

--- test/lego.js
+++ test/lego.js
@@ -13,9 +13,33 @@ function lego() {
 }
 
 async function exec($) {
+  process.env.LEGO_EMAIL = 'stas@ekb.ru'
+  process.env.EXEC_PATH = 'bin/na01'
+  process.env.EXEC_PROPAGATION_TIMEOUT = 300
+  process.env.LEGO_SERVER = Staging
 
+  let domain = `${await random()}.cli.uralhimmash.com`.toLowerCase()
+
+  let child = spawn('lego', [
+    '-a',
+    '-dns', 'exec',
+    '-d', domain,
+    '--pfx',
+    '--dns.resolvers', '8.8.8.8',
+    'run'],
+    { stdio: 'inherit' })
+  let res = await wait(child)
+  $.assert.equal(res, 0)
 }
 
 async function http($) {
 
+}
+
+function wait(child) {
+  return new Promise(function (resolve, reject) {
+    child
+      .on('error', reject)
+      .on('exit', resolve)
+  })
 }
