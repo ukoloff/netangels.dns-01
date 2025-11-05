13e9b351354b9def9df09b0e13f957f854904ceb
Stas Ukolov <ukoloff@gmail.com>
2025-11-05 13:55:34 +0500

    Run self in tests

---
+++ go/lego_test.go
@@ -0,0 +1,43 @@
+package na01_test
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+)
+
+func TestLego(t *testing.T) {
+	me := exePath()
+
+	cmd := exec.Command("go", "build", "-C", "main", "-o", me)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.Remove(me)
+	t.Run("cli", func(t *testing.T) {
+		cmd := exec.Command(me)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("www", func(t *testing.T) {
+
+	})
+}
+
+func exePath() string {
+	me, _ := os.Executable()
+	f, _ := os.CreateTemp("", "na01-*"+filepath.Ext(me))
+	f.Close()
+	os.Remove(f.Name())
+	return f.Name()
+}
