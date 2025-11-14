36746a566e998f8048e664dee1f72bbaf6a9769e
Stas Ukolov <ukoloff@gmail.com>
2025-11-14 10:44:32 +0000

    Extract Web testing

--- go/www_test.go
+++ go/www_test.go
@@ -1,6 +1,7 @@
 package na01_test
 
 import (
+	"errors"
 	"na01"
 	"strings"
 	"testing"
@@ -15,16 +16,23 @@ func TestWWW(t *testing.T) {
 	}()
 	defer na01.StopWWW()
 
+	err := FireWebTest()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func FireWebTest() error {
 	time.Sleep(108 * time.Millisecond)
 
 	err := na01.FireAlive()
 	if err != nil {
-		t.Fatal(err)
+		return err
 	}
 
 	z, err := na01.NewZone("http-" + na01.RandomString(7) + ".ru")
 	if err != nil {
-		t.Fatal(err)
+		return err
 	}
 	defer na01.DropZone(z.ID)
 
@@ -33,36 +41,37 @@ func TestWWW(t *testing.T) {
 
 	rr, err := na01.FirePresent(fqdn, text)
 	if err != nil {
-		t.Fatal(err)
+		return err
 	}
 	if rr.Name != fqdn || rr.Data["value"] != text {
-		t.Fatal("got incorrect RR")
+		return errors.New("got incorrect RR")
 	}
 
 	rrs, err := na01.FindRRs(fqdn)
 	if err != nil {
-		t.Fatal(err)
+		return err
 	}
 	if len(rrs) != 1 {
-		t.Fatal("new RR not found")
+		return errors.New("new RR not found")
 	}
 	if rrs[0].Name != fqdn || rrs[0].Data["value"] != text {
-		t.Fatal("incorrect RR found")
+		return errors.New("incorrect RR found")
 	}
 
 	rrs, err = na01.FireCleanUp(fqdn, text)
 	if err != nil {
-		t.Fatal(err)
+		return err
 	}
 	if rrs[0].Name != fqdn || rrs[0].Data["value"] != text {
-		t.Fatal("incorrect RR deleted")
+		return errors.New("incorrect RR deleted")
 	}
 
 	rrs, err = na01.FindRRs(fqdn)
 	if err != nil {
-		t.Fatal(err)
+		return err
 	}
 	if len(rrs) != 0 {
-		t.Fatal("new RR has not been deleted")
+		return errors.New("new RR has not been deleted")
 	}
+	return nil
 }
